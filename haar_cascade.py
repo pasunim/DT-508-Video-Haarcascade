6506dc6687068c1a92634315d4c3659836fb9449
pasunim <pasunim@users.noreply.github.com>
2026-06-27 13:42:04 +0700

    Initial commit

---
+++ haar_cascade.py
@@ -0,0 +1,44 @@
+import cv2
+
+cap = cv2.VideoCapture("videos/ryan.mp4")
+# cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier("dataset/haarcascade_frontalface_default.xml")
+smile_cascade = cv2.CascadeClassifier("dataset/haarcascade_smile.xml")
+smile_count = 0
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for x, y, w, h in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        roi_gray = gray[y : y + h, x : x + w]
+        roi_color = frame[y : y + h, x : x + w]
+
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
+
+        for sx, sy, sw, sh in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx + sw, sy + sh), (0, 255, 0), 2)
+            smile_count += 1
+
+    cv2.putText(
+        frame,
+        f"Smiles: {smile_count}",
+        (10, 30),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        2,
+    )
+    cv2.imshow("Smile Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
